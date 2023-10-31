bf8df6614a8f84f700aced1ac00cbc0870001560
Rogeliogamer <130010925+Rogeliogamer@users.noreply.github.com>
2023-10-31 11:48:24 -0600

    Add files via upload

---
+++ Aplicacion, Base de Datos y JAR's 6/Aplicacion, Base de Datos y JAR's/ProyectoConsultorio_PDF/ProyectoConsultorio_PDF/build/classes/jFrames/RegistroAlumnos.rs
@@ -0,0 +1,7 @@
+ventanas.RegistroAlumnos$3
+ventanas.RegistroAlumnos$2
+ventanas.RegistroAlumnos
+ventanas.RegistroAlumnos$5
+ventanas.RegistroAlumnos$4
+ventanas.RegistroAlumnos$6
+ventanas.RegistroAlumnos$1
